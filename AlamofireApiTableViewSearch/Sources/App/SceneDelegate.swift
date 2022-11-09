857b4cc54dc247ba5508ffac533e690b9161955f
Siarheimasiuk <winst73@gmail.com>
2022-11-09 14:12:00 +0300

    AppDelegate changes

--- AlamofireApiTableViewSearch/Sources/App/SceneDelegate.swift
+++ AlamofireApiTableViewSearch/Sources/App/SceneDelegate.swift
@@ -1,22 +1,16 @@
+////
+////  SceneDelegate.swift
+////  AlamofireApiTableViewSearch
+////
+////  Created by Sergio on 2.11.22.
+////
 //
-//  SceneDelegate.swift
-//  AlamofireApiTableViewSearch
+//import UIKit
 //
-//  Created by Sergio on 2.11.22.
+//@main
+//class SceneDelegate: UIResponder, UIApplicationDelegate {
 //
-
-import UIKit
-
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
-    var window: UIWindow?
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        let viewController = MainViewController()
-        let navigationController = UINavigationController(rootViewController: viewController)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
-    }
-}
+//    var window: UIWindow?
+//
+//    
+//}
